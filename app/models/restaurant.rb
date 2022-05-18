e309c209e642cb458027569431a370168112d819
liz tobin <lizmtobin@gmail.com>
2022-05-18 15:48:26 +0100

    add relations & validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+
+  validates :name, presence :true, unique: true
+  validates :description, presence :true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
+
+  validates :content, length: { minimum: 10 }
 end
