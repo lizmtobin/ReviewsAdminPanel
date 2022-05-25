6b2069bc89263b5e7269684afff483e65258f2d5
liz tobin <lizmtobin@gmail.com>
2022-05-25 20:34:54 +0100

    validation update

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -6,5 +6,13 @@ ActiveAdmin.register Restaurant do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :name, :description, :user_id
+
+    index do
+    selectable_column
+    column :id
+    column :name
+    column :created_at
+    actions
+  end
   
 end
